6e6d033a4df40d508adb364b3225e5e135070c6f
Ripal Patel <ripal212007@gmail.com>
2018-10-22 21:34:44 -0500

    everything working

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -1,5 +1,6 @@
 import { loadBoards } from '../data/boardsData.js';
 import {initialPinView} from './pins.js';
+import { loadPinsOnBoards } from '../data/pinsData.js';
 
 const bindEvents = () => {
     $('#user-boards').on('click', '.board-card', (e)=>{
@@ -15,26 +16,40 @@ const bindEvents = () => {
 const writeBoards = (boards)=>{
     let domString = '';
     boards.forEach(board => {
+        const boardImg = board.pins[0] ? board.pins[0].image_url : './db/default-img.jpeg';
         domString += `
         <div id= '${board.id}' class="board-card p-2">
-            <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
+            <img class="card-img-top" src="${boardImg}" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">${board.name}</h5>
-              <p class="card-text">42 Pins</p>
+              <p class="card-text">${board.pins.length}</p>
             </div>
         </div>
         `
     });
     $('#user-boards').html(domString);
 }
+
+// const initialBoardView = ()=>{
+//     loadBoards().then((boards)=>{
+//         // console.log('inside the .then');
+//         writeBoards(boards);
+//         bindEvents();
+//     }).catch((error)=>{
+//         console.error(error);
+//     });
+// }
+
 const initialBoardView = ()=>{
     loadBoards().then((boards)=>{
         // console.log('inside the .then');
-        writeBoards(boards);
+        return loadPinsOnBoards(boards);
+    }).then((boardsWithPins)=> {
+
+        writeBoards(boardsWithPins);
         bindEvents();
     }).catch((error)=>{
         console.error(error);
     });
 }
-
 export{initialBoardView};
--- javascripts/data/pinsData.js
+++ javascripts/data/pinsData.js
@@ -14,4 +14,22 @@ const loadPinsForBoard = (boardID) => {
     })
 }
 
-export {loadPinsForBoard};
+const loadPinsOnBoards = (boards) => {
+  return new Promise ((resolve, reject)=>{
+    $.get('../db/pins.json')
+    .done((data)=>{
+      // var x =[1,2,3].map(num => num*2) // [2,4,6]
+      const boardsWithPins = boards.map(board=>{
+        const matchingPins = data.pins.filter(pin => pin.board_id === board.id);
+        board.pins = matchingPins;
+        return board;
+      })
+      resolve(boardsWithPins);
+    })
+    .fail ((error)=>{
+      reject('error loadPinsOnBoards', error);
+    })
+})
+}
+
+export {loadPinsForBoard, loadPinsOnBoards};
